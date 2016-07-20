f1986247afc9f6b1d38f3169a876819cc0cbaf6a
zousunying <zousunying@meituan.com>
2016-07-20 20:37:47 +0800

    recover print

--- mosclient/common/client.py
+++ mosclient/common/client.py
@@ -78,8 +78,6 @@ class BaseClient(object):
 
 
     def _request(self, **kwargs):
-        for key in kwargs:
-            print"keyword arg: %s: %s" % (key, kwargs[key])
         params = {}
         params['Action'] = self._get_action(3)
         params['AWSAccessKeyId'] = self.access
